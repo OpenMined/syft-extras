e6ae12d3bfff314680f4adc740a7111dc07fc9f5
eelcovdw <eelcovdw@gmail.com>
2025-03-19 10:02:05 +0100

    clean up example

--- examples/ollama_http/client.py
+++ examples/ollama_http/client.py
@@ -5,6 +5,8 @@ This example demonstrates how to directly talk to Ollama, proxied through syft-h
 import httpx
 from syft_http_bridge import create_syft_http_client
 
+# The syft_client below can be directly swapped with the commented out direct_client
+# The direct client communicates over http, while the syft_client communicates over syft filesync
 # ollama_url = "http://localhost:11434"
 # direct_client = httpx.Client(base_url=ollama_url)
 
@@ -12,22 +14,26 @@ app_name = "ollama"
 host = "eelco@openmined.org"
 syft_client = create_syft_http_client(app_name, host)
 
+query = "Explain differential privacy in 1 paragraph."
 response = syft_client.post(
     "/api/generate",
     json={
         "model": "llama3.2:1b",
-        "prompt": "Explain differential privacy in 1 paragraph.",
+        "prompt": query,
         "stream": False,
     },
     timeout=60,
 )
 
-print(response.json()["response"])
+print("Query:", query)
+print("Ollama Response:", response.json()["response"])
 print()
 print()
 
+# Query disallowed endpoint returns a 403 Forbidden
 response = syft_client.get(
     "/api/admin/health",
 )
 
-print(response.status_code, response.json())
+assert response.status_code == 403
+print("Disallowed endpoint returned:", response.status_code, response.json())
--- examples/ollama_http/ollama_app.py
+++ examples/ollama_http/ollama_app.py
@@ -1,20 +1,36 @@
+"""
+Before running, make sure Ollama is installed and running on http://localhost:11434 (default).
+See: https://ollama.com/download
+
+This example will use llama3.2:1b to generate text based on a prompt in client.py.
+To download the model first, run: `ollama pull llama3.2:1b`
+
+The script can also be run as a CLI application using the command:
+```
+pip install "syft-http-bridge[cli]"
+syft-http-bridge \
+    --app-name ollama \
+    --base-url http://localhost:11434 \
+    --allowed-endpoints /api/generate \
+    --allowed-endpoints /api/chat
+```
+"""
+
 import httpx
 from syft_http_bridge import SyftHttpBridge
 
-ollama_url = "http://localhost:11434"
-
-
-def run_server():
-    http_client = httpx.Client(base_url=ollama_url)
-
-    ollama_bridge = SyftHttpBridge(
-        app_name="ollama",
-        http_client=http_client,
-        allowed_endpoints=["/api/generate"],
-    )
-
-    ollama_bridge.run_forever()
+OLLAMA_URL = "http://localhost:11434"
 
 
 if __name__ == "__main__":
-    run_server()
+    http_client = httpx.Client(base_url=OLLAMA_URL)
+
+    # Only allow /api/generate and /api/chat endpoints
+    # Other Ollama endpoints will return a 403 Forbidden
+    ollama_bridge = SyftHttpBridge(
+        app_name="ollama",
+        http_client=http_client,
+        allowed_endpoints=["/api/generate", "/api/chat"],
+    )
+
+    ollama_bridge.run_forever()
