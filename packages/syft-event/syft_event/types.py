2039e2e1d2e0112dfc509f365853100c329b0d92
Shubham Gupta <11032835+shubham3121@users.noreply.github.com>
2025-05-29 23:14:45 +0530

    syft_events: remove params parameter from SyftRequest

--- packages/syft-event/syft_event/types.py
+++ packages/syft-event/syft_event/types.py
@@ -11,7 +11,6 @@ class Request(BaseModel):
     url: SyftBoxURL
     headers: Dict[str, str] = Field(default_factory=dict)
     body: Optional[bytes]
-    params: Optional[Dict[str, str]] = Field(default_factory=dict)
 
 
 class Response(BaseModel):
