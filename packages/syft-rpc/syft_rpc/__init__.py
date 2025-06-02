244685fc7233f917b6b6df32caf7414d2da5af67
Khoa Duy Nguyen <88959106+khoaguin@users.noreply.github.com>
2025-06-02 15:45:03 +0700

    bump syft-rpc to 0.2.1
    Co-authored-by: rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>

--- packages/syft-rpc/syft_rpc/__init__.py
+++ packages/syft-rpc/syft_rpc/__init__.py
@@ -1,7 +1,7 @@
 from .protocol import SyftBulkFuture, SyftFuture, SyftRequest, SyftResponse
 from .rpc import broadcast, reply_to, send
 
-__version__ = "0.1.0"
+__version__ = "0.2.1"
 
 __all__ = [
     "broadcast",
