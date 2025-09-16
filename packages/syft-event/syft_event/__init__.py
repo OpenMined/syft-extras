2ebb3f83782154eb61c798f94523cdf2ed9190a5
Shubham Gupta <11032835+shubham3121@users.noreply.github.com>
2025-09-16 10:45:34 +0000

    bump: version 0.3.0 → 0.4.0

--- packages/syft-event/syft_event/__init__.py
+++ packages/syft-event/syft_event/__init__.py
@@ -2,5 +2,5 @@ from .router import EventRouter
 from .server2 import SyftEvents
 from .types import Request, Response
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __all__ = ["SyftEvents", "Request", "Response", "EventRouter"]
