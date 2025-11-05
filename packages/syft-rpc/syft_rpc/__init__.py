b357c5088038d78171339c48cbb63513b61d54d8
Khoa Nguyen <88959106+khoaguin@users.noreply.github.com>
2025-11-05 03:11:35 +0000

    bump: version 0.4.0 → 0.4.1

--- packages/syft-rpc/syft_rpc/__init__.py
+++ packages/syft-rpc/syft_rpc/__init__.py
@@ -1,7 +1,7 @@
 from .protocol import SyftBulkFuture, SyftFuture, SyftRequest, SyftResponse
 from .rpc import broadcast, reply_to, send
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 __all__ = [
     "broadcast",
